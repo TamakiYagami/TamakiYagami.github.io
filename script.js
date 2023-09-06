00813b9c73568466454a87acb7cad5659600c8b6
TamakiYagami <enfants54@live.fr>
2023-09-06 16:30:07 +0200

    Commit

--- script.js
+++ script.js
@@ -65,6 +65,55 @@ function montre() {
     }
 } 
 
-setInterval(montre, 2000)
+// setInterval(montre, 2000)
 
+// Array = Tableau
+// Type de variable qui est elle même un tableau
+//          0      1       2    3
+var tab = [
+    10, // 0
+    "bonjour",  // 1
+    7.5,  // 2
+    null // 3
+]
+// Cette variable est tableau qui contient 4 valeurs dans l'ordre 
+// 10
+// "bonjour"
+// 7.5
+// null
+console.log(tab[1]) 
+// On affiche la valeur qui ce trouve à la position 1 qui est "bonjour"
+console.log(tab[3])
+// On affiche la valeur qui ce trouve à la position 3 qui est null
 
+// Je voudrait un tableau qui ce nomme Chmilblik qui comporte 
+// 5 valeur de type string et 5 valeur de type int ou float
+
+let Chmilblik = [
+    "Bonjour",
+    "Je",
+    "suis",
+    "très",
+    "fort",
+    10,
+    4.5,
+    70,
+    1151,
+    45.56548
+]
+console.log(Chmilblik)
+console.log(Chmilblik.length)
+
+// getElemntById séléctionne un élement qui à l'id défini sur 
+//animal dans ce cas
+// addEventListener créer une écoute d'évenement 
+let animal = "Autruche"
+let temp = ""
+document.getElementById('animal').addEventListener('click', function() {
+    // Je regarde le texte qui ce trouve dans cette élément 
+    temp = document.getElementById('animal').innerHTML 
+    // Je modifie le texte qui ce trouve dans cette élément par la valeur
+    // de la variable animal 
+    document.getElementById('animal').innerHTML = animal
+    animal = temp
+})
