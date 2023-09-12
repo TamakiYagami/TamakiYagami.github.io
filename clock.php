90ceca0a9fac9da99a7a3a162218de7af4ed6771
TamakiYagami <enfants54@live.fr>
2023-09-12 16:29:02 +0200

    Commit all

---
+++ clock.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Horloge</title>
+</head>
+<body>
+    <?php 
+    ?>
+    <!-- Faite une horloge sois numérique sois analogique
+    On doit pouvoir changer l'heure avec un formulaire -->
+</body>
+</html>
--- flexbox.php
+++ flexbox.php
@@ -44,6 +44,32 @@
         </div>
     </div>
 
+    <div class="flexbox">
+        <div class="cote_gauche">
+            <div class="long violet"></div>
+            <div class="gauche">
+                <div class="petite Orange"></div>
+                <div class="trespetite red"></div>
+            </div>
+            <div class="droite bleu"></div>
+        </div>
+    
+        <div class="cote_droit">            
+            <div class="gauche">
+                <div class="petite1 red"></div>
+                <div class="petite2 bleu"></div>
+                <div class="petite3 Orange"></div>
+            </div>
+            <div class="droite">
+                <div class="petite1 bleu"></div>
+                <div class="petite2 Orange"></div>
+                <div class="petite3 red"></div>
+            </div>
+            <div class="long violet"></div>
+        </div>
+        <h1>CSS FLEXBOX</h1>
+    </div>
+
 
 
 
