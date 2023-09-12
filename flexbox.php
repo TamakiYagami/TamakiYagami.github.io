0a67ec2f9ea0eff8ca89c90abbbbe097d3299fb5
TamakiYagami <enfants54@live.fr>
2023-09-12 13:08:23 +0200

    Commit all

--- flexbox.php
+++ flexbox.php
@@ -19,7 +19,34 @@
         <div class="vert"></div>
         <div class="rouge"></div>
         <div class="orange"></div>  
+        <div class="rouge"></div>
+        <div class="rouge"></div>
+        <div class="rouge"></div>
+        <div class="rouge"></div>
+        <div class="orange"></div>  
+        <div class="vert"></div>
+        <div class="rouge"></div>
+        <div class="orange"></div>  
+        <div class="vert"></div>
+
 
     </div>
+
+    <div class="grand">
+        <div class="petit">
+            <h1>FlexBox</h1>
+        </div>
+        <div class="petit" id="secondpetit">
+            <div class="moyen"></div>
+            <div class="toutpetit"></div>
+            <div class="toutpetit"></div>
+            <div class="moyen"></div>
+        </div>
+    </div>
+
+
+
+
+
 </body>
 </html>
