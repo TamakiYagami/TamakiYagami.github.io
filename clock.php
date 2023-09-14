a8a240254992ed85090e909ec9136c5ed14f063f
TamakiYagami <enfants54@live.fr>
2023-09-14 11:36:12 +0200

    Commit

--- clock.php
+++ clock.php
@@ -15,38 +15,44 @@
         <input type="number" name="minute" id="minute" value=0 min=0 max=59 lenght=2>
 
         <label for="seconde">Seconde(s) :</label>
-        <input type="number" name="seconde" id="seconde" value=15 min=0 max=59 lenght=2>
+        <input type="number" name="seconde" id="seconde" value=0 min=0 max=59 lenght=2>
 
-        <input type="submit" value="Régler">
+        <input type="submit" value="Régler">    
+        <button type="button" id="pause" onclick="PauseTimer()">Pause</button>
+        <button type="button" id="play">Play</button>
     </form>
+
 
     <?php 
         echo '<script>
         var heure = ' . (!empty($_POST["heure"]) ? $_POST["heure"] : '0') . '
         var minute = ' . (!empty($_POST["minute"]) ? $_POST["minute"] : '0') . '
-        var seconde = ' . (!empty($_POST["seconde"]) ? $_POST["seconde"] : '15') . '
-        setInterval(function() {            
+        var seconde = ' . (!empty($_POST["seconde"]) ? $_POST["seconde"] : '0') . '
+        var interval = setInterval(function() {            
             document.getElementById("minuteur").innerHTML= 
             `${(heure < 10 ? "0" : "") + heure}:${(minute < 10 ? "0" : "") + minute}:${(seconde < 10 ? "0" : "") + seconde}`
-            if (seconde <= 0) return
-            seconde--
+            if (seconde <= 0 && minute <= 0 && heure <= 0 ) return
+
             if (seconde == 0) {
                 if (minute > 0) {
-                    seconde = 59
+                    seconde = 60
                     minute--
                 } else {
                     if (heure > 0) {
                         heure--
                         minute = 59
-                        seconde = 59
+                        seconde = 60
                     }
                 }
             }
-            // console.log(heure, minute, seconde)
+            seconde--
         },  1000)
+        function PauseTimer() {
+            clearInterval(interval)
+            document.getElementById("minuteur").style.opacity = "0"
+        }
     </script>';
     ?>
-    
     <!-- Faite un minuteur numérique 
     On doit pouvoir changer le temps qu'il dur
      avec un formulaire -->
