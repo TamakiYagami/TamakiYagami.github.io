4289758be3b32237e71dcedb251ec2e584851d03
TamakiYagami <enfants54@live.fr>
2023-09-14 16:18:05 +0200

    commit

--- clock.php
+++ clock.php
@@ -20,6 +20,7 @@
         <input type="submit" value="Régler">    
         <button type="button" id="pause" onclick="PauseTimer()">Pause</button>
         <button type="button" id="play">Play</button>
+        <button type="button" id="reset" onclick="ResetTimer()">Reset</button>
     </form>
 
 
@@ -28,9 +29,12 @@
         var heure = ' . (!empty($_POST["heure"]) ? $_POST["heure"] : '0') . '
         var minute = ' . (!empty($_POST["minute"]) ? $_POST["minute"] : '0') . '
         var seconde = ' . (!empty($_POST["seconde"]) ? $_POST["seconde"] : '0') . '
-        var interval = setInterval(function() {            
+        function affichage() {
             document.getElementById("minuteur").innerHTML= 
             `${(heure < 10 ? "0" : "") + heure}:${(minute < 10 ? "0" : "") + minute}:${(seconde < 10 ? "0" : "") + seconde}`
+        }
+        function timer() {
+            affichage()            
             if (seconde <= 0 && minute <= 0 && heure <= 0 ) return
 
             if (seconde == 0) {
@@ -46,11 +50,24 @@
                 }
             }
             seconde--
-        },  1000)
+        }
+        var interval = setInterval(timer,  1000)
         function PauseTimer() {
             clearInterval(interval)
-            document.getElementById("minuteur").style.opacity = "0"
+            interval = null
+            document.getElementById("minuteur").style.opacity = "0.5"
         }
+        function ResetTimer() {
+            heure = 0 
+            minute = 0
+            seconde = 0
+        }
+        document.getElementById("play").addEventListener("click", function() {
+            if (interval == null) {
+                interval = setInterval(timer, 1000)
+                document.getElementById("minuteur").style.opacity = "1"
+            }
+        })
     </script>';
     ?>
     <!-- Faite un minuteur numérique 
