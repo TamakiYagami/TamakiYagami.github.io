711f1568258d7d3e8b3c15d1b0452abc02649bf8
TamakiYagami <enfants54@live.fr>
2023-09-15 16:01:48 +0200

    Commit

---
+++ tableauJO.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Tableau JO</title>
+    <link rel="stylesheet" href="tableaujo.css">
+</head>
+<body>
+    &copy;
+    <table>
+        <thead>
+            <tr>
+                <th colspan=7>Jeux Olympique de Rio 2016</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <th colspan=3>Pays</th>
+                <th colspan=1><img src="./img/or.png" alt="or"></th>
+                <th colspan=1><img src="./img/argent.png" alt="argent"></th>
+                <th colspan=1><img src="./img/bronze.png" alt="bronze"></th>
+                <th colspan=1><img src="./img/total2.png" alt="total"></th>
+            </tr>
+            <tr>
+                <th>1</th>
+                <td class="drapeau"><img src="./img/usa.png" alt="Drapeau USA"></td>
+                <td colspan=1>USA</td>
+                <td>46</td>
+                <td>37</td>
+                <td>38</td>
+                <td>121</td>
+            </tr>
+            <tr>
+                <th>2</th>
+                <td class="drapeau"><img src="./img/grande_bretagne.png" alt="Drapeau Grande Bretagne"></td>
+                <td colspan=1>Grande-Bretagne</td>
+                <td>27</td>
+                <td>23</td>
+                <td>17</td>
+                <td>67</td>
+            </tr>
+            <tr>
+                <th>3</th>
+                <td class="drapeau"><img src="./img/chine.png" alt="Drapeau Chine"></td>
+                <td colspan=1>Chine</td>
+                <td>26</td>
+                <td>18</td>
+                <td>26</td>
+                <td>70</td>
+            </tr>
+            <tr>
+                <th>4</th>
+                <td class="drapeau"><img src="./img/russie.png" alt="Drapeau Russie"></td>
+                <td colspan=1>Russie</td>
+                <td>19</td>
+                <td>18</td>
+                <td>19</td>
+                <td>56</td>
+            </tr>
+            <tr>
+                <th>5</th>
+                <td class="drapeau"><img src="./img/allemagne.png" alt="Drapeau Allemagne"></td>
+                <td colspan=1>Allemagne</td>
+                <td>17</td>
+                <td>10</td>
+                <td>15</td>
+                <td>42</td>
+            </tr>
+            <tr>
+                <th>6</th>
+                <td class="drapeau"><img src="./img/japon.png" alt="Drapeau Japon"></td>
+                <td colspan=1>Japon</td>
+                <td>12</td>
+                <td>8</td>
+                <td>21</td>
+                <td>41</td>
+            </tr>
+            <tr>
+                <th>7</th>
+                <td class="drapeau"><img src="./img/france.png" alt="Drapeau France"></td>
+                <td colspan=1>France</td>
+                <td>10</td>
+                <td>18</td>
+                <td>14</td>
+                <td>42</td>
+            </tr>
+        </tbody>
+
+        <tfoot>
+            <tr>
+                <th colspan=7>Ce classement est limité aux 7 premières nations</th>
+            </tr>
+        </tfoot>
+    </table>
+</body>
+</html>
