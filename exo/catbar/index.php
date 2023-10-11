062dd81a677d99c3c3a6fa2701ce5c89ae87cadb
TamakiYagami <enfants54@live.fr>
2023-10-11 16:25:30 +0200

    commit

--- exo/catbar/index.php
+++ exo/catbar/index.php
@@ -1,6 +1,6 @@
 <?php 
 session_start();
-require_once('../../function/dbCat.php');
+    require_once('../../function/dbCat.php');
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -26,23 +26,21 @@ require_once('../../function/dbCat.php');
             <form method="post">
                 <h2>Faire une réservation (15 min)</h2>
                 <label for="cat">Chat :</label>
-                <!-- <datalist id="cat">
-                    <option value="Ronron"></option>
-                    <option value="Lily"></option>
-                    <option value="Pompette"></option>
-                    <option value="Garfield"></option>
-                </datalist>
-                <input list='cat'> -->
                 <select name="cat" id="catSelect" required>
                     <?php
-                        $select = $bdd->prepare('SELECT * FROM cat WHERE reserver=0 AND veto=0 AND transfer=0'); 
-                        // Je séléctionne toute les colonnes et les lignes qui on la colonne reserver, veto et transfer à 0
-                        $select->execute();
-                        $select = $select->fetchAll();
-                        if (!empty($select)) { // Je vérifie que j'ai des lignes qui ont était récuperer
-                            for ($i=0; $i < count($select); $i++) { // Je fait une boucle qui tourne le nombre de ligne récupérer
-                                echo "<option value='" . $select[$i]['id'] . "'>" . $select[$i]['prenom'] . "</option>";
-                            }
+                        $selectCat = $bdd->prepare('SELECT * FROM cat WHERE veto=0 AND transfer=0'); 
+                        // Je séléctionne toute les colonnes et les lignes qui on la colonne veto et transfer à 0
+                        $selectCat->execute();
+                        $selectCat = $selectCat->fetchAll();
+                        if (!empty($selectCat)) { // Je vérifie que j'ai des lignes qui ont était récuperer
+                            $selectRes = $bdd->prepare('SELECT * FROM reservation');
+                            $selectRes->execute();
+                            $selectRes = $selectRes->fetchAll();
+
+                            
+                            // for ($i=0; $i < count($select); $i++) { // Je fait une boucle qui tourne le nombre de ligne récupérer
+                            //     echo "<option value='" . $select[$i]['id'] . "'>" . $select[$i]['prenom'] . "</option>";
+                            // }
                         }
                     ?>
                 </select>
@@ -55,12 +53,48 @@ require_once('../../function/dbCat.php');
                     <option value="5">5</option>
                 </select>
                 <label for="date">Date :</label>
-                <input type="datetime-local" name="date" id="date" required 
-                
+                <input type="datetime-local" name="date" id="date" oninput="ChangeDate()"  
                     value="<?php echo date('Y-n-d'); ?>T<?php echo date('H')+2; ?>:00" 
                     min="<?php echo date('Y-n-d'); ?>T08:00" 
-                    max="<?php echo date('Y-n')?>-<?php echo date('d')+7; ?>T19:00"> 
-                    <!-- Ici je récupère la date d'aujourd'hui pour pouvoir définir value, min, max -->
+                    step="900"
+                    max="<?php echo date('Y-n')?>-<?php echo date('d')+7?>T18:59"
+                required>                     
+                <!-- Ici je récupère la date d'aujourd'hui pour pouvoir définir value, min, max -->
+                <script>
+                    function ChangeDate() {
+                        let Element = document.getElementById('date')
+                        // Split est une fonction de JavaScript qui permet de découper une chaine de caractère
+                        // Au caractère que on lui donne et il va enfaite nous créer un tableau
+                        let temporaire = Element.value.split('T')
+                        // parseInt est une fonction de JavaScript qui permet de convertir tout type de variable
+                        // en entier 
+                        if (parseInt(temporaire[1]) > 19) {
+                            Element.setCustomValidity('A cette heure on est fermé')
+                        } else {
+                            Element.setCustomValidity('')
+                        }
+                        let theo = [
+                            '00',
+                            '15',
+                            '30',
+                            '45'
+                        ]
+                        // let heure  = temporaire[1].split(':')[0]
+                        let minute = temporaire[1].split(':')[1]
+                        // let date = new Date(); // new Date est défini par JavaScript est récupère toute
+                        // // les informations du moment et d'aujourd'hui
+
+                        theo.forEach(function(lavabo) {
+                            if (minute === lavabo) {
+                                
+                            }
+                        })
+
+
+                        
+                
+                    }
+                </script>
                 <label for="comment">Commentaire :</label>
                 <textarea name="comment" id="comment" cols="30" rows="5" maxlength='255'></textarea>
                 <input type="submit" value="Faire ma réservation">
@@ -72,12 +106,15 @@ require_once('../../function/dbCat.php');
             // J'insert dans la base de donnée les inforamtions que je lui est donnée dans le formulaire
             $insert = $bdd->prepare('INSERT INTO reservation(id_client, id_cat, date, comment, tab) VALUES (?, ?, ?, ?, ?)');
             $insert->execute(array(
+                // Le (int) va passer la variable temporairement en entier 
                 (int)$_SESSION['id'],
                 (int)$_POST['cat'],
                 str_replace('T', ' ', $_POST['date']),
                 $_POST['comment'],
                 (int)$_POST['table']
             ));
+ 
+            header('Location: index.php');
         }
         ?>
     <?php endif; ?>
@@ -88,3 +125,6 @@ require_once('../../function/dbCat.php');
     <?php include 'inc/footer.php'; ?>
 </body>
 </html>
+
+
+
